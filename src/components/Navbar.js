97222d8f5244f682db15d1924c51e42e4150a53c
Paul Lee <pauljlee94@gmail.com>
2019-06-15 00:57:44 -0400

    Add navbar background color on scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React from "react"
 import "./Navbar.css"
 
-function Navbar() {
-    return (
-        <div className="navbar">
-            <h1><a href="#">Monogram Projects</a></h1>
-            <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Property</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-    )
+class Navbar extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            opacity: "0"
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('scroll', () => {
+            this.setState({opacity: window.scrollY/300})
+        })
+    }
+
+    render() {
+        return (
+            <div className="navbar" style={{backgroundColor: "rgba(0,0,0," + this.state.opacity + ")"}}>
+                <h1><a href="#">Monogram Projects</a></h1>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Property</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Navbar
